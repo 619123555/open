bee53ed1bedeb75156e1816660aae2177ec1e2e3
619123555 <619123555@qq.com>
2021-02-20 11:37:49 +0800

    修改mybatis自动生成代码配置

--- boss/src/main/java/com/trans/payment/boss/entity/PaymentOrder.java
+++ boss/src/main/java/com/trans/payment/boss/entity/PaymentOrder.java
@@ -1,6 +1,7 @@
 package com.trans.payment.boss.entity;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Id;
@@ -12,70 +13,70 @@ import lombok.Data;
 public class PaymentOrder implements Serializable {
     @Id
     @Column(name = "id")
-    private Long id;
+    private String id;
 
     @Column(name = "version")
     private Integer version;
 
     @Column(name = "pay_type")
-    private String pay_type;
+    private String payType;
 
     @Column(name = "trade_no")
-    private String trade_no;
+    private String tradeNo;
 
     @Column(name = "amount")
-    private Long amount;
+    private BigDecimal amount;
 
     @Column(name = "status")
     private String status;
 
     @Column(name = "create_time")
-    private Date create_time;
+    private Date createTime;
 
     @Column(name = "complete_time")
-    private Date complete_time;
+    private Date completeTime;
 
     @Column(name = "currency")
     private String currency;
 
     @Column(name = "payer_acc_no")
-    private String payer_acc_no;
+    private String payerAccNo;
 
     @Column(name = "payer_acc_name")
-    private String payer_acc_name;
+    private String payerAccName;
 
     @Column(name = "payee_acc_no")
-    private String payee_acc_no;
+    private String payeeAccNo;
 
     @Column(name = "payee_acc_name")
-    private String payee_acc_name;
+    private String payeeAccName;
 
     @Column(name = "payee_acc_type")
-    private String payee_acc_type;
+    private String payeeAccType;
 
     @Column(name = "payee_cert_type")
-    private String payee_cert_type;
+    private String payeeCertType;
 
     @Column(name = "payee_cert_no")
-    private String payee_cert_no;
+    private String payeeCertNo;
 
     @Column(name = "payee_phone")
-    private String payee_phone;
+    private String payeePhone;
 
     @Column(name = "payee_bank_code")
-    private String payee_bank_code;
+    private String payeeBankCode;
 
     @Column(name = "payee_bank_name")
-    private String payee_bank_name;
+    private String payeeBankName;
 
     @Column(name = "remark")
     private String remark;
 
     @Column(name = "channel_id")
-    private String channel_id;
+    private String channelId;
 
     @Column(name = "channel_trade_no")
-    private String channel_trade_no;
+    private String channelTradeNo;
 
     private static final long serialVersionUID = 1L;
 }
