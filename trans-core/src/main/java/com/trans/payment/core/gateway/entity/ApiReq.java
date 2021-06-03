5f50c7d0b15dcfb65de39ca9908f2a7d2f820f0a
619123555 <619123555@qq.com>
2021-06-03 17:26:05 +0800

    参数验证器bug优化

--- trans-core/src/main/java/com/trans/payment/core/gateway/entity/ApiReq.java
+++ trans-core/src/main/java/com/trans/payment/core/gateway/entity/ApiReq.java
@@ -1,10 +1,10 @@
 package com.trans.payment.core.gateway.entity;
 
 import java.io.Serializable;
-import javax.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * 网关通用参数
