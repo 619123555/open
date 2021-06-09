a98813304b9a058932df6dd3261f814c9a7efead
619123555 <619123555@qq.com>
2021-06-09 17:23:10 +0800

    添加redisson

---
+++ open-common/src/main/java/com/open/common/configuration/RedissonProperties.java
@@ -0,0 +1,27 @@
+package com.open.common.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.redis.redisson")
+public class RedissonProperties {
+
+    private String config;
+
+    private String file;
+
+    public String getConfig() {
+        return config;
+    }
+
+    public void setConfig(String config) {
+        this.config = config;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+}
